050c801e0c881066cdc5c6820f58009a47d34086
kiragoo <zhangl910201@163.com>
2020-08-03 20:47:49 +0800

    【ADD】 如果是 非synchronized(this) 则代码块中的是异步执行,且没有调用同一个对象的监控器

---
+++ src/com/kirago/cp02/demo13/ThreadA.java
@@ -0,0 +1,15 @@
+package com.kirago.cp02.demo13;
+
+public class ThreadA extends Thread{
+    private Service service;
+    
+    public ThreadA(Service service){
+        super();
+        this.service = service;
+    }
+    
+    @Override
+    public void run(){
+        service.setUsernameAndPassword("a", "aa");
+    }
+}
---
+++ src/test/com/kirago/cp02/demo13/ServiceTest.java
@@ -0,0 +1,26 @@
+package com.kirago.cp02.demo13;
+
+
+
+import org.junit.Test;
+
+public class ServiceTest {
+    @Test
+    public void run(){
+        Service service = new Service();
+        ThreadA threadA = new ThreadA(service);
+        threadA.setName("A");
+        threadA.start();
+
+        ThreadB threadB = new ThreadB(service);
+        threadB.setName("B");
+        threadB.start();
+
+        try {
+            Thread.sleep(10000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+}
